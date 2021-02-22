ff638b2ab66fdff2cd8a5f4743d98bc95caa14e1
Ashwani <76509193+ashwani9008@users.noreply.github.com>
2021-02-22 14:12:10 +0530

    added auth

---
+++ client/src/components/Auth/Auth.js
@@ -0,0 +1,14 @@
+import React from 'react';
+// import useStyles from './styles';
+
+const Auth = () => {
+    //const state = null;
+
+    return (
+        <div>
+            <h1>This is Auth</h1>
+        </div>
+    )
+}
+
+export default Auth
